dca965dfed2b93f5876ce3d82f262c62f89406ba
Marcelle Santos <marcelledossantos2@gmail.com>
2023-12-06 20:38:08 -0300

    Config

---
+++ src/main/java/com/PotterTravelAPI/PotterTravelAPI/PotterTravelApiApplication.java
@@ -0,0 +1,13 @@
+package com.PotterTravelAPI.PotterTravelAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotterTravelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotterTravelApiApplication.class, args);
+	}
+
+}
