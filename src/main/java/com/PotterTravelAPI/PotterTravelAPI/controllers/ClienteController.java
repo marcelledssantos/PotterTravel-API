3c027522bddeb13d9fd25be183fc991b30808d46
Marcelle Santos <marcelledossantos2@gmail.com>
2023-12-06 22:59:49 -0300

    add clienteController

--- src/main/java/com/PotterTravelAPI/PotterTravelAPI/controllers/ClienteController.java
+++ src/main/java/com/PotterTravelAPI/PotterTravelAPI/controllers/ClienteController.java
@@ -4,10 +4,10 @@ import com.PotterTravelAPI.PotterTravelAPI.entities.Cliente;
 import com.PotterTravelAPI.PotterTravelAPI.services.ClienteService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Clientes", description = "CRUD")
 @RestController
@@ -22,4 +22,32 @@ public class ClienteController {
         return clienteService.saveCliente(cliente);
     }
 
+    @GetMapping("/allclientes")
+    public List<Cliente> getAllClientes() {
+
+        return clienteService.getAllClientes();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Long id) {
+        Cliente cliente = clienteService.getClienteById(id);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable Long id, @RequestBody Cliente clienteUpdated) {
+       Cliente cliente = clienteService.getClienteById(id);
+
+        cliente.setNome(clienteUpdated.getNome());
+
+        clienteService.saveCliente(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCliente(@PathVariable Long id) {
+        clienteService.deleteById(id);
+    }
 }
